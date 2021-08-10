010d0e8fdf3046e724d839bf4cd1e54c6863b4d0
SMccarrick <smccarrick2804@gmail.com>
2021-08-10 20:25:15 +0100

    intitial config setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+    theme: {
+        colors: {
+            // Transparent: 'transparent',
+            // current: 'currentColor',
+            // white: '#F0F4F8',
+            // green: '#194A50',
+            // background: '#102A43',
+            // subBackground: '#334E68',
+            // orange: '#D8A28C'
+        },
+    },
+};
